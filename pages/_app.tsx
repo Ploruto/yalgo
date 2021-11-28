0f94d04ed250f70670ac79c6f1e4224549f5008f
Philipp Haack <philippHaackCode@gmail.com>
2021-11-28 15:38:27 +0100

    added default Navbar component & added in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import 'styles/styles.css'
+import Navbar from 'components/Navbar'
 
 // eslint-disable-next-line
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <>
+
+        <Navbar />
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
